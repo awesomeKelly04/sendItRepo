e2941dbbe9d540a6e130297cd9b73c6d1b9639a7
Emmanuel Oboh <[emmanueloboh04@gmail.com]>
2018-11-11 13:40:38 +0100

    Starting Over again from UI

---
+++ scripts/parcelSummary.js
@@ -0,0 +1,19 @@
+function parcelSummary(){
+	window.location.replace("parcelSummary.html");
+	return false;
+}
+
+function changeDestination(){
+	window.location.replace("parcelSummary.html");
+	return false;
+}
+
+function cancelOrder(){
+	window.location.replace("displayAllOrderByUserView.html");
+	return false;
+}
+
+function changeLocation(){
+	window.location.replace("adminPage.html");
+	return false;
+}
